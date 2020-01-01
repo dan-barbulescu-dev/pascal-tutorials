777b230b002699611fbb4f69086601ee749f4504
Dan Bărbulescu <danbarbulescu.main@gmail.com>
2020-01-01 04:00:08 +0200

    Update sieve_of_eratosthenes.pas

--- sieve_of_eratosthenes.pas
+++ sieve_of_eratosthenes.pas
@@ -1,4 +1,6 @@
-{This Pascal program finds out all the prime numbers up to a number N, by using a very efficient algorithm called The Sieve of Eratosthenes. 
+{Sieve of Eratosthenes – finding out all prime numbers up to N
+
+This Pascal program finds out all the prime numbers up to a number N, by using a very efficient algorithm called The Sieve of Eratosthenes. 
 How it works in short is that it goes through each number up to N, and if that number is prime, it marks all of its multiples as not prime. 
 Thus, only the prime numbers are left unmarked and easy to recognize.}
 
